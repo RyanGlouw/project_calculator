4ae79eaed85e0d81abff6bbac15566f8427e47cd
ryantodd <ryantodd1176@gmail.com>
2021-05-16 01:37:34 +0300

    projectDone

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 "use strict"
 
-function insert(num){
+function insert(num) {
   document.form.textview.value = document.form.textview.value + num;
+}
+
+function clean() {
+  document.form.textview.value = "";
+}
+
+function back() {
+  let deleteValues = document.form.textview.value;
+  document.form.textview.value = deleteValues.substring(0, deleteValues.length - 1);
+}
+
+function equal() {
+  let values = document.form.textview.value;
+  if (values) {
+    values = document.form.textview.value = eval(values);
+  }
 }
